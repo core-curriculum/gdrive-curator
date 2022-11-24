24d9438847feaebfb9becc22859ae08f37c2d837
Takeshi Kondo <ncukondo@gmail.com>
2022-11-24 09:48:17 +0000

    fix directory structure

--- src/lib/utils.py
+++ src/lib/utils.py
@@ -1,4 +1,5 @@
 import os
+from os import path
 import glob
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
@@ -7,12 +8,19 @@ DOCS_DIR= os.path.join(BASE_DIR,"docs")
 DOCS_OUTCOMES_DIR= os.path.join(DOCS_DIR,"outcomes")
 SHEETS_OUTCOMES_DIR= os.path.join(SHEETS_DIR,"outcomes")
 SHEETS_GENERAL_DIR= os.path.join(SHEETS_DIR,"general")
-OUTPUT_DIR= os.path.join(BASE_DIR,"output","2022","ja")
+OUTPUT_BASE_DIR= os.path.join(BASE_DIR,"output")
+OUTPUT_RELATIONS_DIR = os.path.join(OUTPUT_BASE_DIR,"relations","from2016to2022")
+OUTPUT_DIR= os.path.join(OUTPUT_BASE_DIR,"2022","ja")
+OUTPUT_2016_DIR= os.path.join(OUTPUT_BASE_DIR,"2016")
 OUTPUT_OUTCOMES_DIR= os.path.join(OUTPUT_DIR,"outcomes")
 OUTPUT_OUTCOMES_TABLE_DIR= os.path.join(OUTPUT_OUTCOMES_DIR,"tables")
 OUTCOME_TABLE_SOURCE_DIR= os.path.join(SHEETS_DIR,"outcome_tables_source")
-OUTPUT_RELATIONS_DIR = os.path.join(OUTPUT_DIR,"relations","from2016to2022")
 TABLE_FORMATTED_DIR= os.path.join(OUTPUT_DIR,"tables")
+
+DOCS_SOURCE_DIR = path.join(DOCS_DIR,"documents")
+OUTPUT_DOCS_DIR = path.join(OUTPUT_DIR,"documents")
+OUTPUT_DOCS_CONTENTS_DIR = path.join(OUTPUT_DOCS_DIR,"contents")
+
 
 def get_glob_file(glob_path:str)->str:
     files = glob.glob(glob_path)
