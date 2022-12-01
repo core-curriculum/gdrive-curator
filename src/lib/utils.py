def626b8277b207da165aa9ddc53606ce891ffea
Takeshi Kondo <ncukondo@gmail.com>
2022-12-01 08:15:03 +0000

    minor fix

--- src/lib/utils.py
+++ src/lib/utils.py
@@ -1,6 +1,7 @@
 import os
 from os import path
 import glob
+import pandas as pd
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 SHEETS_DIR= os.path.join(BASE_DIR,"sheets")
@@ -29,3 +30,32 @@ def get_glob_file(glob_path:str)->str:
     return list(glob.glob(glob_path))[0]
 
 
+def save_csv(data:pd.DataFrame,file_path:str):
+    """
+    save dataframe as csv file
+    """
+    data.to_csv(file_path,index=False,encoding="utf_8_sig")
+
+def load_csv(file_path:str):
+    """
+    load dataframe as csv file
+    """
+    return pd.read_csv(file_path,encoding="utf_8_sig")
+
+
+def save_text(text:str,file_path:str):
+    """
+    save text as text file
+    """
+    with open(file_path,"w",encoding="utf_8") as f:
+        f.write(text)
+
+
+def load_text(file_path:str):
+    """
+    load text file
+    """
+    text=""
+    with open(file_path,"r",encoding="utf_8") as f:
+        text = f.read()
+    return text
